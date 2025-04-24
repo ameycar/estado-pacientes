43d3997df70f172fee5b348d0e929b16a57403d8
ameycar <camilacostamejias@gmail.com>
2025-04-24 07:03:55 -0500

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Configuración de Firebase
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  databaseURL: "TU_DATABASE_URL",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyAX2VYw2XVs6DGsw38rCFaSbk3VuUA60y4",
+  authDomain: "estado-pacientes.firebaseapp.com",
+  databaseURL: "https://estado-pacientes-default-rtdb.firebaseio.com",
+  projectId: "estado-pacientes",
+  storageBucket: "estado-pacientes.firebasestorage.app",
+  messagingSenderId: "515522648971",
+  appId: "1:515522648971:web:d7b6e9cde4a7d36181ad8e",
+  measurementId: "G-C9STJV4J6K"
 };
 
 firebase.initializeApp(firebaseConfig);
