f3e14ae1d1f9de803428769431966c9067bf337a
ameycar <camilacostamejias@gmail.com>
2025-09-24 18:10:28 -0500

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA25H1n0isws9Jw8-LvOvqql2M1cEfzFaU",
+  authDomain: "estado-pacientes-staging.firebaseapp.com",
+  projectId: "estado-pacientes-staging",
+  storageBucket: "estado-pacientes-staging.firebasestorage.app",
+  messagingSenderId: "17770566264",
+  appId: "1:17770566264:web:bec6ff862496dc082f538d",
+  measurementId: "G-HCP9F2BB0Y"
+};
 
