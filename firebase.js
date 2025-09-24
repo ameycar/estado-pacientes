49f073bdac7206fdc9d8f836fb7a30c853e39b3c
ameycar <camilacostamejias@gmail.com>
2025-09-24 18:14:50 -0500

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,19 @@
+// Importa lo necesario de Firebase
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+// ⚡ Configuración de tu proyecto STAGING
 const firebaseConfig = {
   apiKey: "AIzaSyA25H1n0isws9Jw8-LvOvqql2M1cEfzFaU",
   authDomain: "estado-pacientes-staging.firebaseapp.com",
   projectId: "estado-pacientes-staging",
-  storageBucket: "estado-pacientes-staging.firebasestorage.app",
+  storageBucket: "estado-pacientes-staging.appspot.com",
   messagingSenderId: "17770566264",
-  appId: "1:17770566264:web:bec6ff862496dc082f538d",
-  measurementId: "G-HCP9F2BB0Y"
+  appId: "1:17770566264:web:bec6ff862496dc082f538d"
 };
 
-
-// Initialize Firebase
+// Inicializa la app de Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Exporta la conexión a la base de datos
+export const db = getDatabase(app);
