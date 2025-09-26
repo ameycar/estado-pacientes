7bd580f3a10c86ba10a214aed3030406c3c2c6d7
ameycar <camilacostamejias@gmail.com>
2025-09-26 17:41:27 -0500

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,20 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+
+// Configuración de Firebase (STAGING o PRODUCCIÓN según quieras probar)
+const firebaseConfig = {
+  apiKey: "AIzaSyA25H1n0isws9Jw8-LvOvqql2M1cEfzFaU",
+  authDomain: "estado-pacientes-staging.firebaseapp.com",
+  projectId: "estado-pacientes-staging",
+  storageBucket: "estado-pacientes-staging.appspot.com",
+  messagingSenderId: "17770566264",
+  appId: "1:17770566264:web:bec6ff862496dc082f538d"
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exporta la base de datos
+export const db = getDatabase(app);
 
